73cd7f32fc59e4e772956db7de65ac21492106d0
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-07-10 21:16:14 +0530

    Singlet Combination checked

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,12 +1,31 @@
-#!/bin/bash 
+#!/bin/bash
 
 echo "Welcome to Flip Coin Simulation"
 
-checkCoin=$((RANDOM%2))
-if [ $checkCoin -eq 0 ]
+function flipCoin () {
+	local numOfFlips=$1 choice=$2
+        for ((i=1; i<=$numOfFlips; i++))
+        do
+	        headsOrTails=""
+                for ((j=1; j<=$choice; j++ ))
+                do
+                        checkCoin=$((RANDOM%2))
+                        if [ $checkCoin -eq 0 ]
+                        then
+                                headsOrTails+=H
+                        else
+                                headsOrTails+=T
+                        fi
+                done
+	done
+}
+
+
+read -p "Dou you want to Flip (y/n)?: " input
+if [[ $input == "y" || $input == "Y" ]]
 then
-	headsOrTails+=H
-else
-	headsOrTails+=T
+	read -p "Enter Number of times you want to Flip a coin: " num_of_flips
+	echo -e "Enter \n1.Singlet Combination"
+	read choice
+	flipCoin $num_of_flips $choice
 fi
-echo $headOrTails
